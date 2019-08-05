1b9a1c0478e88c2e376dfdd355f660e85d1fd6fd
i.shihovcev <shihovcev@metamodel.ru>
2019-08-05 17:41:56 +0500

    chapter 5

---
+++ multithreading/src/main/java/ru/shikhovtsev/javathreadprogramming/сhapter5/BestReplacement.java
@@ -0,0 +1,125 @@
+package ru.shikhovtsev.javathreadprogramming.сhapter5;
+
+import ru.shikhovtsev.javathreadprogramming.chapter17.BooleanLock;
+
+public class BestReplacement {
+  private Thread internalThread;
+  private volatile boolean stopRequested;
+  private BooleanLock suspendRequested;
+  private BooleanLock internalThreadSuspended;
+
+  public BestReplacement() {
+    stopRequested = false;
+
+    suspendRequested = new BooleanLock(false);
+    internalThreadSuspended = new BooleanLock(false);
+
+    Runnable r = () -> {
+      try {
+        runWork();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    };
+
+    internalThread = new Thread(r);
+    internalThread.start();
+  }
+
+  private void runWork() {
+    int count = 0;
+
+    while (!stopRequested) {
+      try {
+        waitWhileSuspended();
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        continue;
+      }
+
+      System.out.println("Part I - count = " + count);
+
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+      System.out.println("Part II - count = " + count);
+
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+
+      System.out.println("Part III - count " + count);
+      count++;
+    }
+  }
+
+  private void waitWhileSuspended() throws InterruptedException {
+    synchronized (suspendRequested) {
+      if (suspendRequested.isTrue()) {
+        try {
+          internalThreadSuspended.setValue(true);
+          suspendRequested.waitUntilFalse(0);
+        } finally {
+          internalThreadSuspended.setValue(false);
+        }
+      }
+    }
+  }
+
+  public void suspendRequest() {
+    suspendRequested.setValue(true);
+  }
+
+  public void resumeRequest() {
+    suspendRequested.setValue(false);
+  }
+
+  public boolean waitForActualSuspension(long msTimeout) throws InterruptedException {
+    return true;
+  }
+
+  public void stopRequest() {
+    stopRequested = true;
+    internalThread.interrupt();
+  }
+
+  public boolean isAlive() {
+    return internalThread.isAlive();
+  }
+
+  public static void main(String[] args) {
+    try {
+      var br = new BestReplacement();
+      System.out.println("---> Just created, br.isAlive() = " + br.isAlive());
+      Thread.sleep(4200);
+
+      long startTime = System.currentTimeMillis();
+      br.suspendRequest();
+      System.out.println("--> just submitted a suspendRequest");
+
+      boolean suspensionTookEffect = br.waitForActualSuspension(10000);
+      long stopTime = System.currentTimeMillis();
+
+      if (suspensionTookEffect) {
+        System.out.println("->> the internall thread tool " + (stopTime - startTime) + " ms to notice");
+      } else {
+        System.out.println("->> the internal thread did not notice");
+      }
+
+      Thread.sleep(5000);
+
+      br.resumeRequest();
+      System.out.println("just submitted a resumeRequest");
+      Thread.sleep(2200);
+
+      br.stopRequest();
+      System.out.println("->> just submitted a stopRequest");
+    } catch (InterruptedException e) {
+
+    }
+  }
+}
---
+++ multithreading/src/main/java/ru/shikhovtsev/javathreadprogramming/сhapter5/DeprecatedSuspendResume.java
@@ -0,0 +1,65 @@
+package ru.shikhovtsev.javathreadprogramming.сhapter5;
+
+public class DeprecatedSuspendResume implements Runnable {
+
+  private volatile int firstVal;
+  private volatile int secondVal;
+
+  public boolean areValuesEqual() {
+    return firstVal == secondVal;
+  }
+
+  public void run() {
+    try {
+      firstVal = 0;
+      secondVal = 0;
+      workMethod();
+    } catch (InterruptedException x) {
+      System.out.println("interrupted while in workMethod()");
+    }
+  }
+
+  private void workMethod() throws InterruptedException {
+    int val = 1;
+
+    while (true) {
+      stepOne(val);
+      stepTwo(val);
+      val++;
+
+      Thread.sleep(200);
+    }
+  }
+
+  private void stepOne(int newVal) throws InterruptedException {
+    firstVal = newVal;
+    Thread.sleep(300);
+  }
+
+  private void stepTwo(int newVal) {
+    secondVal = newVal;
+  }
+
+  public static void main(String[] args) {
+    var dsr = new DeprecatedSuspendResume();
+    Thread t = new Thread(dsr);
+    t.start();
+
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+
+    }
+    for (int i = 0; i < 10; i++) {
+      t.suspend();
+      System.out.println("dsr.areValEqual() " + dsr.areValuesEqual());
+      t.resume();
+      try {
+        Thread.sleep((long) (Math.random() * 2000.0));
+      } catch (InterruptedException e) {
+
+      }
+    }
+  }
+
+}
