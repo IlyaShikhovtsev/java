b7661073a6cca71678e28f560d2639bbf5c2cde2
i.shihovcev <shihovcev@metamodel.ru>
2019-09-19 18:06:07 +0500

    SureStop

--- multithreading/src/main/java/ru/shikhovtsev/javathreadprogramming/chapter16surestop/SureStopVerbose.java
+++ multithreading/src/main/java/ru/shikhovtsev/javathreadprogramming/chapter16surestop/SureStopVerbose.java
@@ -5,7 +5,8 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-public class SureStop {
+public class SureStopVerbose {
+
   private static class Entry {
     private Thread thread;
     private long stopTime;
@@ -16,17 +17,18 @@ public class SureStop {
     }
   }
 
-  private static SureStop ss;
+  private static SureStopVerbose ss;
 
   static {
-    ss = new SureStop();
+    ss = new SureStopVerbose();
+    print("SureStopVerbose instance is created");
   }
 
   private List<Entry> stopList;
   private List<Entry> pendingList;
   private Thread internalThread;
 
-  private SureStop() {
+  private SureStopVerbose() {
     stopList = new LinkedList<>();
 
     pendingList = new ArrayList<>(20);
@@ -38,10 +40,8 @@ public class SureStop {
         e.printStackTrace();
       }
     };
-
     internalThread = new Thread(r);
     internalThread.setDaemon(true);
-
     internalThread.setPriority(Thread.MAX_PRIORITY);
     internalThread.start();
   }
@@ -49,38 +49,55 @@ public class SureStop {
   private void runWork() {
     try {
       while (true) {
+        print("about to sleep for 0.5 seconds");
         Thread.sleep(500);
+        print("done with sleep for 0.5 seconds");
+
         long sleepTime = checkStopList();
+        print("back from checkStopList(), sleepTime=" + sleepTime);
 
         synchronized (pendingList) {
           if (pendingList.size() < 1) {
-            pendingList.wait(sleepTime);
+            print("about to wait on pendingList for " + sleepTime + " ms");
+            long start = System.currentTimeMillis();
+            pendingList.wait();
+            long elapsedTime = System.currentTimeMillis();
+
+            print("waited on pendingList for " + elapsedTime + " ms");
           }
 
           if (pendingList.size() > 0) {
+            print("copying " + pendingList.size() + " elements from pendingList to stopList");
+
+            int oldSize = stopList.size();
             stopList.addAll(pendingList);
             pendingList.clear();
+            int newSize = stopList.size();
+            print("pendingList.size()=" + pendingList.size() + ", stopList grew by " + (newSize - oldSize));
           }
         }
       }
     } catch (InterruptedException e) {
-
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
   private long checkStopList() {
+    print("entering checkStop() - stopList.size()=" + stopList.size());
+
     long currTime = System.currentTimeMillis();
     long minTime = Long.MAX_VALUE;
 
     Iterator<Entry> iter = stopList.iterator();
-
     while (iter.hasNext()) {
       Entry entry = iter.next();
 
       if (entry.thread.isAlive()) {
+        print ("thread is alive - " + entry.thread.getName());
         if (entry.stopTime < currTime) {
+          print ("timed out, stopping - " + entry.thread.getName());
+
           try {
             entry.thread.stop();
           } catch (SecurityException e) {
@@ -91,8 +108,10 @@ public class SureStop {
           iter.remove();
         } else {
           minTime = Math.min(entry.stopTime, minTime);
+          print("new minTime = " + minTime);
         }
       } else {
+        print ("thread died on its own - " + entry.thread.getName());
         iter.remove();
       }
     }
@@ -101,6 +120,7 @@ public class SureStop {
 
     sleepTime = Math.max(50, sleepTime);
 
+    print("leaving checkStopList() - stopList.size() = " + stopList.size());
     return sleepTime;
   }
 
@@ -109,17 +129,31 @@ public class SureStop {
       pendingList.add(entry);
 
       pendingList.notify();
+      print("added entry to pendingList, name=" + entry.thread.getName() + ", stopTime = " + entry.stopTime + ", in " + (entry.stopTime - System.currentTimeMillis()) +" ms");
     }
   }
 
-  public static void ensureStop(Thread t, long msGracePeriod) {
+  public static void ensureStop (Thread t, long msGracePeriod) {
+    print ("entering ensureStop() - name=" + t.getName() + ", msGracePeriod=" + msGracePeriod);
+
     if (!t.isAlive()) {
+      print("already stopped, not added to list = " + t.getName());
       return;
     }
 
     long stopTime = System.currentTimeMillis() + msGracePeriod;
-
     Entry entry = new Entry(t, stopTime);
     ss.addEntry(entry);
+    print("leaving ensureStop() - name= " + t.getName());
+  }
+
+  private static void print(String msg) {
+    Thread t = Thread.currentThread();
+    String name = t.getName();
+    if (t == ss.internalThread) {
+      name = "SureStopThread";
+    }
+
+    System.out.println(name +": " + msg);
   }
 }
